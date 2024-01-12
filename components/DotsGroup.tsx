9b8ed50fd10357904e5b5556f2910aa8d6355669
Marco Mojica <marco@mojica.de>
2024-01-12 17:55:16 +0100

    Add AudioFiles and playback on selection

--- components/DotsGroup.tsx
+++ components/DotsGroup.tsx
@@ -3,9 +3,46 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion-3d";
 
+import {
+  CameraControls,
+  PositionalAudio,
+  Text,
+  Billboard,
+  useSelect,
+} from "@react-three/drei";
 import { GroupProps, useFrame, useThree } from "@react-three/fiber";
-import { Vector3 } from "three";
-import { CameraControls, useSelect, Html } from "@react-three/drei";
+import { Vector3, PositionalAudio as PositionalAudioImpl } from "three";
+
+const sounds = [
+  "Arthur",
+  "Carlo",
+  "Chris 2",
+  "Gina",
+  "Jordan",
+  "Linh 1",
+  "Marco",
+  "Marvin",
+  "Max 2",
+  "Naima",
+  "Seda",
+  "Sophie H.",
+  "Tien",
+  "Vicky",
+  "Ben",
+  "Chris 1",
+  "Gamze",
+  "Jo-Ann",
+  "Leeloo",
+  "Linh 2",
+  "Marie",
+  "Max 1",
+  "Michael",
+  "Piet",
+  "Sofiy",
+  "Sophie R.",
+  "Timo",
+  "Vivien",
+];
 
 export default function DotsGroup() {
   const controls = useThree(
@@ -23,7 +60,7 @@ export default function DotsGroup() {
   });
 
   const positions = useMemo(
-    () => Array.from({ length: 30 }).map(() => ({ ...getPoint() })),
+    () => sounds.map((filename) => ({ src: filename, ...getPoint() })),
     []
   );
 
@@ -35,6 +72,12 @@ export default function DotsGroup() {
 
   useEffect(() => {
     if (selected[0]) {
+      const audio = selected[0].children.filter(
+        (child) => child.type === "Audio"
+      )[0] as PositionalAudioImpl;
+
+      audio?.play();
+
       const position = new Vector3();
       selected[0].getWorldPosition(position);
       controls?.setLookAt(
@@ -46,6 +89,7 @@ export default function DotsGroup() {
         position.z,
         true
       );
+
       setRotate(false);
     } else {
       controls?.reset(true);
@@ -56,8 +100,10 @@ export default function DotsGroup() {
   return (
     <motion.group ref={groupRef} castShadow receiveShadow>
       <AnimatePresence>
-        {positions.map((position, i) => (
+        {positions.map(({ src, ...position }, i) => (
           <motion.mesh
+            castShadow
+            receiveShadow
             userData={{ dot: true }}
             key={"why me?" + i}
             initial={{
@@ -68,7 +114,8 @@ export default function DotsGroup() {
               ...position,
               transition: {
                 type: "spring",
-                duration: 3,
+                duration: 5,
+                delay: 0.8,
                 scale: {
                   repeat: Infinity,
                   repeatType: "reverse",
@@ -82,20 +129,27 @@ export default function DotsGroup() {
               color={"#fff"}
               roughness={0.2}
               metalness={0.1}
-              transparent
-              opacity={0.99}
             />
+            <PositionalAudio
+              url={`/assets/sound/${src}.mp3`}
+              distance={1}
+              autoplay={false}
+              loop={false}
+            />
+            {/* <motion.pointLight position={[0, 0, 0]} intensity={10} /> */}
+            <Billboard>
+              <Text
+                depthOffset={0}
+                color="white"
+                anchorX={"center"}
+                position={[0, 1.6, 0]}
+              >
+                {src}
+              </Text>
+            </Billboard>
           </motion.mesh>
         ))}
       </AnimatePresence>
-      {/* <Html
-        as="div" // Wrapping element (default: 'div')
-        transform // If true, applies matrix3d transformations (default=false)
-        position={[100, 0, 0]} // The x/y/z of the object relative to the center of the scene (default=[0,0,0])
-      >
-        <h1 className="text-white">hello</h1>
-        <p>world</p>
-      </Html> */}
     </motion.group>
   );
 }
