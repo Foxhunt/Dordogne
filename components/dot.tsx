33f327626a7c6cfd5d778f130550d6dde6e47a4f
Marco Mojica <marco@mojica.de>
2024-01-20 15:55:07 +0800

    Refactor dot audio/text sync and update ring animations

--- components/dot.tsx
+++ components/dot.tsx
@@ -1,4 +1,5 @@
 import { Billboard, Text, useSelect } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion-3d";
 import { useEffect, useRef, useState } from "react";
@@ -38,13 +39,10 @@ interface Transcript {
   languageCode: string;
 }
 
-// The whole file can be represented as an array of Transcripts
-type TranscriptFile = Transcript[];
-
 export default function Dot({ filename, position }: DotProps) {
   const meshRef = useRef<Object3D>(null!);
   const audioRef = useRef<HTMLAudioElement>(null!);
-  const wordsRef = useRef<TransformedWord[]>(null!);
+  const wordsRef = useRef<TransformedWord[]>([]);
 
   const [word, setWord] = useState("");
 
@@ -53,12 +51,13 @@ export default function Dot({ filename, position }: DotProps) {
       const audio = new Audio(`/assets/sound/${filename}.mp3`);
       audio.loop = false;
       audio.autoplay = false;
-      audio.preload = "metadata";
+      audio.preload = "auto";
 
       audioRef.current = audio;
 
-      fetch(`/assets/transcripts/${filename}.json`).then((response) => {
-        response.json().then((data: { results: TranscriptFile }) => {
+      fetch(`/assets/transcripts/${filename}.json`)
+        .then((response) => response.json())
+        .then((data: { results: Transcript[] }) => {
           const words = data.results
             .flatMap((result) => result.alternatives[0].words)
             .map(({ word, startOffset, endOffset }) => ({
@@ -68,68 +67,57 @@ export default function Dot({ filename, position }: DotProps) {
             }));
           wordsRef.current = words;
         });
-      });
     }
   }, [filename]);
 
+  const selected = useSelect();
+  const [isSelected, setIsSelected] = useState(selected[0] === meshRef.current);
   useEffect(() => {
-    function onTimeUpdate() {
-      const currentTime = audioRef.current.currentTime;
-      const currentWord = wordsRef.current.find(
-        ({ startOffset, endOffset }) =>
-          currentTime >= startOffset && currentTime <= endOffset,
-      );
-      if (currentWord && currentWord.word !== word) {
-        setWord(currentWord.word);
-      }
-    }
-
-    audioRef.current.addEventListener("timeupdate", onTimeUpdate);
-
-    return () => {
-      audioRef.current.removeEventListener("timeupdate", onTimeUpdate);
-    };
-  }, [word]);
+    setIsSelected(selected[0] === meshRef.current);
+  }, [selected]);
 
   const [visited, setVisited] = useState(false);
-
-  const isSelected = useSelect()[0] === meshRef.current;
   useEffect(() => {
     const audio = audioRef.current;
+    console.log(isSelected);
     if (isSelected) {
       audio.load();
       audio.play();
       setVisited(true);
     } else if (!audio.paused) {
       audio.pause();
-      setWord("");
     }
-  }, [isSelected]);
+  }, [filename, isSelected]);
 
-  console.log(word);
+  const timeRef = useRef(0);
+
+  useFrame((state, delta) => {
+    if (isSelected) {
+      const currentTime = (timeRef.current += delta) - 0.3;
+      const currentWord = wordsRef.current.find(
+        ({ startOffset, endOffset }) =>
+          currentTime >= startOffset && currentTime <= endOffset,
+      );
+      if (currentWord) {
+        setWord(currentWord.word);
+      }
+    } else {
+      timeRef.current = 0;
+    }
+  });
 
   return (
     <motion.mesh
       //@ts-ignore
       ref={meshRef}
-      castShadow
-      receiveShadow
       userData={{ dot: true }}
-      initial={{
-        scale: 1,
-      }}
+      position={[position.x, position.y, position.z]}
       animate={{
-        scale: 1 + 0.2 * Math.random(),
-        ...position,
+        // ...position,
         transition: {
           type: "spring",
           duration: 5,
           delay: 0.8,
-          scale: {
-            repeat: Infinity,
-            repeatType: "reverse",
-            duration: 0.7 + 0.3 * Math.random(),
-          },
         },
       }}
     >
@@ -138,48 +126,89 @@ export default function Dot({ filename, position }: DotProps) {
         color={"#fff"}
         roughness={0.2}
         metalness={0.1}
+        transparent
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: {
+            duration: 5,
+          },
+        }}
       />
       <Billboard>
-        <Text color="white" anchorX={"center"} position={[0, 1.6, 0]}>
-          {filename}
-        </Text>
         <AnimatePresence>
-          <motion.group
-            key={word}
-            initial={{ x: 0, y: 0, z: 0, scale: 0, opacity: 0 }}
-            animate={{ x: 1.5, y: 0.5, z: 3, scale: 1, opacity: 1 }}
-            exit={{ x: 4, y: 2, z: -10, scale: 0, opacity: 0 }}
-          >
-            {isSelected && (
+          {isSelected && (
+            <motion.group
+              key={word}
+              initial={{ x: 0, y: 0, z: 0, scale: 0 }}
+              animate={{ x: 1.5, y: 0.5, z: 3, scale: 1 }}
+              exit={{ x: 2, y: 2, z: -3, scale: 0 }}
+            >
               <Text color="white" anchorX={"left"}>
                 {word}
               </Text>
-            )}
-          </motion.group>
+            </motion.group>
+          )}
+          {(isSelected || visited) && (
+            <motion.group
+              key={filename}
+              initial={{ x: 0, y: 0, z: 0, scale: 0 }}
+              animate={{ x: 0, y: 1.6, z: 0, scale: 1 }}
+              exit={{ x: 0, y: 0, z: 0, scale: 0 }}
+            >
+              <Text color="white" anchorX={"center"}>
+                {filename}
+              </Text>
+            </motion.group>
+          )}
+          {!visited &&
+            new Array(5).fill("").map((_, index) => (
+              <motion.mesh
+                key={"ring" + index}
+                rotation={[Math.PI, 0, 0]}
+                variants={{
+                  initial: { scale: 2 },
+                  animate: {
+                    scale: 0,
+                    transition: {
+                      duration: 3,
+                      repeatDelay: 1,
+                      delay: 2 + index * 0.2,
+                      repeat: Infinity,
+                      repeatType: "loop",
+                    },
+                  },
+                  exit: { scale: 0, transition: { duration: 1 } },
+                }}
+                initial={"initial"}
+                animate={"animate"}
+                exit={"exit"}
+              >
+                <ringGeometry args={[1, 1.1]} />
+                <motion.meshStandardMaterial
+                  color={"#fff"}
+                  roughness={0.2}
+                  metalness={0.1}
+                  side={DoubleSide}
+                  transparent
+                  variants={{
+                    initial: { opacity: 0 },
+                    animate: {
+                      opacity: 1,
+                      transition: {
+                        duration: 3,
+                        repeatDelay: 1,
+                        delay: 2 + index * 0.2,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                      },
+                    },
+                    exit: { opacity: 0, transition: { duration: 1 } },
+                  }}
+                />
+              </motion.mesh>
+            ))}
         </AnimatePresence>
-        {!visited && (
-          <motion.mesh
-            rotation={[(Math.PI / 2) * 0.1, 0, 0]}
-            initial={{ scale: 1.3 }}
-            animate={{
-              scale: 1,
-              transition: {
-                duration: 4 + Math.random() * 2,
-                repeatDelay: Math.random() * 3,
-                repeat: Infinity,
-                repeatType: "reverse",
-              },
-            }}
-          >
-            <ringGeometry args={[2, 2.1]} />
-            <meshStandardMaterial
-              color={"#fff"}
-              roughness={0.2}
-              metalness={0.1}
-              side={DoubleSide}
-            />
-          </motion.mesh>
-        )}
       </Billboard>
     </motion.mesh>
   );
